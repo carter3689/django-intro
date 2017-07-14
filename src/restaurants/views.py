117e821e99c586add1ac5d5973c6631dbe7ee9cd
joel carter <joel carter>
2017-07-14 17:56:57 -0500

    Adding newest changes to the django experiment project

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,19 +1,14 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 # Function based views
 def home(request):
-    html_ = """<!DOCTYPE html>
-    <html lang = en>
-
-    <head>
-    </head>
-    <body>
-    <h1> Hello World </h1>
-    <p> This is html coming through here.</p>
-    </body>
-    </html>
-    """
-    return HttpResponse(html_)
-    #return render(request, "home.html", {})#response
+    num = random.randint(0,10000000)
+    some_list = [random.randint(0,200000),random.randint(0,3000000)]
+    context = {"num": num,
+    "bool_item":True,
+    "some_item":some_list
+    }
+    return render(request, "base.html",context )#response
